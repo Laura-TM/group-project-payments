dd63fd98c02aadb0f1db5e1ecbd4345bce465598
Laura-TM <lauratmenendez@outlook.com>
2021-06-18 22:09:46 +0100

    completing balance on the exchange rate field

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Balance from "./components/Balance";
 import CalcPayment from "./components/CalcPayment";
 import Payments from "./components/Payments";
 import currencies from "./data/currencies";
+import rates from "./data/rates";
 import PendingPaymentTable from "./components/PendingPaymentTable";
 import "./App.css";
 
@@ -11,6 +12,7 @@ class App extends Component {
     super();
     this.state = {
       currencies: currencies,
+      rates: rates,
       balance: 87.43 // This is the current balance in GBP
     };
   }
@@ -21,7 +23,11 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Payments</h1>
         </header>
-        <Balance total={this.state.balance} currencies={this.state.currencies} />
+        <Balance
+          total={this.state.balance}
+          currencies={this.state.currencies}
+          rates={this.state.rates}
+        />
         <CalcPayment currencies={this.state.currencies} />
         <h2>Completed Payments</h2>
         <Payments />
--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,33 +1,35 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./Balance.css";
 
-class Balance extends Component {
-  constructor() {
-    super();
-    this.state = {
-      alternateCurrency: "USD"
-    };
+function Balance(props) {
+  const [alternateCurrency, setAlternateCurrency] = useState("USD");
+  let initialConvertedBalance = props.rates[alternateCurrency] * props.total;
+  const [convertedBalance, setConvertedBalance] = useState(initialConvertedBalance);
+
+  function convert(selectedCurrency) {
+    const conversionRate = props.rates[selectedCurrency];
+    const newConvertedBalance = conversionRate * props.total;
+    setAlternateCurrency(selectedCurrency);
+    setConvertedBalance(newConvertedBalance);
   }
 
-  render() {
-    return (
-      <div className="Balance">
-        <h2 className="Balance-title">
-          Your account balance is
-          <span className="Balance-total">£{this.props.total}</span>
-        </h2>
-        <div className="Balance-alt">
-          Your balance is ??? in
-          <select defaultValue={this.state.alternateCurrency}>
-            {this.props.currencies.map((currency, index) => (
-              <option key={index}>{currency}</option>
-            ))}
-          </select>
-          .
-        </div>
+  return (
+    <div className="Balance">
+      <h2 className="Balance-title">
+        Your account balance is
+        <span className="Balance-total">£{props.total}</span>
+      </h2>
+      <div className="Balance-alt">
+        Your balance is {convertedBalance.toFixed(2)} in
+        <select defaultValue={alternateCurrency} onChange={(e) => convert(e.currentTarget.value)}>
+          {props.currencies.map((currency, index) => (
+            <option key={index}>{currency}</option>
+          ))}
+        </select>
+        .
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Balance;
